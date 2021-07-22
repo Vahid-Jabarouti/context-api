dd70feaa3c5f670ea219036d1fb0048743c91f02
iamvahid <vahid.jabarouti@gmail.com>
2021-07-22 20:55:42 +0430

    Adding context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class BookList extends Component {
-
-  render() {
-    
-    
+  render() { 
+    return ( 
+      <div className="book-list">
+        <ul>
+          <li>the way of kings</li>
+          <li>the name of the wind</li>
+          <li>the final empire</li>
+        </ul>
+      </div>
+    );
   }
 }
-
-export default BookList
+ 
+export default BookList;
