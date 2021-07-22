6f02500e4fa165ae60c4d551379cbe2f2a9cc9ec
iamvahid <vahid.jabarouti@gmail.com>
2021-07-22 14:38:48 +0430

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <h1>Context APp</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>COntact</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
